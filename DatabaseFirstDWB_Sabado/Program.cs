1ae523ff353ee06b637e940c8a895b30488ac105
Rolando <rolando.fcfm2020@gmail.com>
2021-03-06 13:33:09 -0600

    before refactor

--- DatabaseFirstDWB_Sabado/Program.cs
+++ DatabaseFirstDWB_Sabado/Program.cs
@@ -7,10 +7,11 @@ namespace DatabaseFirstDWB_Sabado
     class Program
     {
 
+        public static NORTHWNDContext dataContext = new NORTHWNDContext();
         public static void Excercise1()
         {
             //select *from Employees
-            NORTHWNDContext dataContext = new NORTHWNDContext();
+           
 
             // var employeeQry = dataContext.Employees.AsQueryable();
             var employeeQry = dataContext.Employees.Select(s => s);
@@ -25,10 +26,7 @@ namespace DatabaseFirstDWB_Sabado
         public static void Excercise2()
         {
             //SELECT Title, FirstName, LastName FROM Employees WHERE Title = 'Sales Representative';
-
-            var dbContext = new NORTHWNDContext();
-
-            var employeeQry = dbContext.Employees.Select(s => new
+            var employeeQry = dataContext.Employees.Select(s => new
             {
                 s.Title,
                 s.FirstName,
@@ -51,11 +49,11 @@ namespace DatabaseFirstDWB_Sabado
             //Objecto o clase anonima
             //var employee = new { Name = "Rolando", Age = 21 };
             //SELECT FirstName as Nombre, LastName as Apellido, Title as Puesto  FROM Employees WHERE Title<> 'Sales Representative'
-            var dbContext = new NORTHWNDContext();
+           
 
             // A un objeto lambda que se le asigna sobre un ubjeto anonimo o 
             // un objeto no anonimo, se le llama proyección
-            var employeeQry = dbContext.Employees.Where(w => w.Title != "Sales Representative").Select(s => new
+            var employeeQry = dataContext.Employees.Where(w => w.Title != "Sales Representative").Select(s => new
             {
                 Nombre = s.FirstName,
                 Apellido = s.LastName,
@@ -65,11 +63,89 @@ namespace DatabaseFirstDWB_Sabado
             var output = employeeQry.ToList();
 
         }
+
+        public static void Excercise4(int id = 1)
+        {
+            //UPDATE Employees SET NAME = ‘Alejandra’ WHERE ID = 1;
+
+            Employee currentEmployee = GetEmployeeById(id);
+
+            if (currentEmployee == null)
+                throw new Exception("No se encontró el empleado con el ID proporcionado");
+
+            currentEmployee.FirstName = "Alejandra";
+            dataContext.SaveChanges();
+
+
+        }
+
+        private static Employee GetEmployeeById(int id)
+        {
+            return dataContext.
+                Employees.Where(w => w.EmployeeId == id).FirstOrDefault();
+        }
+
+        private static IQueryable<Employee> GetEmployeeByName(string name = "Rolando")
+        {
+            return dataContext.
+                Employees.Where(w => w.FirstName == name).AsQueryable();
+        }
+
+        public static void Excercise5()
+        {
+
+            //  Insertar nuevo producto en la tabla Products
+            var newProduct = new Product();
+            newProduct.ProductName = "Jugo del valle 1lt";
+            newProduct.UnitPrice = 15.50m;
+
+            dataContext.Products.Add(newProduct);
+            dataContext.SaveChanges();
+        }
+
+        public static void Excercise6(int id = 13)
+        {
+            //Borrar un empleado, por el ID
+
+            var employee = GetEmployeeById(id);
+            dataContext.Employees.Remove(employee);
+            dataContext.SaveChanges();
+
+            //Si queremos borrar todos los rolandos
+            //var employees = GetEmployeeByName("Rolando").ToList();
+            //dataContext.Employees.RemoveRange(employees);
+            //dataContext.SaveChanges();
+
+        }
+
+        public static void Excersice7(int orderID = 10248)
+        {
+
+            var qry = dataContext.Orders.Where(w => w.OrderId == orderID)
+                .Select(s => new
+                {
+                    Cliente = s.Customer.CompanyName,
+                    Vendedor = s.Employee.FirstName,
+                    Productos = s.OrderDetails.Select(se => se.Product.ProductName),
+                });
+
+            var result = qry.ToList();
+            //Obtener los productos, el cliente y el empleado por Id de Order
+        }
+
+
         static void Main(string[] args)
         {
             //Excercise1();
             //Excercise2();
-            Excercise3();
+            //Excercise3();
+            //Excercise4(id: 4);
+            //Excercise4();
+            //Excercise5();
+            // Excercise6();
+             Excersice7();
+            //
+
             Console.WriteLine("Hello World!");
         }
     }
